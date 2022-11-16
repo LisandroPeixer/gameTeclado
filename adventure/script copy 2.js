8f5bfd93345a7bf4bb825d4737987bec92926c8a
LISANDRO PEIXER <56092210+LisandroPeixer@users.noreply.github.com>
2022-11-16 00:33:04 -0300

    adventure
    fases concluidas e sistema de compra pra finalizar no copy 2

--- adventure/script copy 2.js
+++ adventure/script copy 2.js
@@ -1,11 +1,23 @@
 const canvas = document.getElementById('canvas');
-canvas.width = 1100; //window.innerWidth;
+canvas.width = 1200; //window.innerWidth;
 canvas.height = 600 //window.innerHeight;
 const ctx = canvas.getContext('2d');
 let colisao = false;
 let torres = 2;
 let argolas = 2;
 let cps = 2;
+let velo = -5;
+let compra = 0;
+let troca = 0;
+let vidas = 0;
+
+alert(	"Bem vindo ao ADVENTURE!!!"+"\n\n"+
+		"Neste jogo, Voce é um aventureiro"+"\n"+
+		"E terá que passar por varios lugares perigosos"+"\n"+
+		"Coletando itens valiosos"+"\n"+
+		"Esquivando-se das armadilhas"+"\n"+
+		"Entre pro nosso ranking de melhores jogadores"+"\n\n"+
+		"USE as SETAS + ESPACO para pular")
 
 // conexao localstorage
 if(localStorage.hasOwnProperty('bdAdv')){
@@ -21,12 +33,9 @@ if(localStorage.hasOwnProperty('bdAdv')){
 	]
 window.localStorage.setItem('bdAdv', JSON.stringify(tabela));
 }
-
-alert("USE A TECLA ESPACO PARA FLUTUAR")
-
 //_4_/////////////////////////////// objetos
 	class obj {
-		constructor(cor, x, y, larg, alt, raio, vel, nome, res, placar){
+		constructor(cor, x, y, larg, alt, raio, vel, nome, res, placar, vida){
 			this.cor = cor;
 			this.x = x;
 			this.y = y;
@@ -35,126 +44,240 @@ alert("USE A TECLA ESPACO PARA FLUTUAR")
 			this.raio = raio;
 			this.vel = vel;
 			this.nome = nome;
-			this.res = res;	
+			this.res = res;
 			this.placar = placar;
+			this.vida = vida
 		}
 		retangulo(){
-			ctx.fillStyle = this.cor;                                     
-			ctx.fillRect(this.x, this.y, this.larg, this.alt); 
+			ctx.fillStyle = this.cor;
+			ctx.fillRect(this.x, this.y, this.larg, this.alt);
 			return ctx
 		}
 		circulo(){
-			ctx.fillStyle = this.cor;  
-			ctx.beginPath();  
-			ctx.arc(this.x, this.y, this.raio, 0, Math.PI * 2, true);    // x, y, raio, angulo, fimAngulo, rotacao)  
+			ctx.fillStyle = this.cor;
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.raio, 0, Math.PI * 2, true);    // x, y, raio, angulo, fimAngulo, rotacao)
 			ctx.fill();
 			return ctx;
 		}
 		text(){                         // função para desenhar placar
-			ctx.fillStyle = this.cor;  
-			ctx.font = '35px sans-serif';  
-			ctx.fillText(this.nome, this.x+100, this.y);
-			ctx.fillText(this.res, this.x, this.y);
-			ctx.fillText(this.placar, this.x, this.y+50);
+			ctx.fillStyle = this.cor;
+			ctx.font = '35px sans-serif';
+			ctx.fillText(p1.vida,	 	this.x,		this.y);
+			ctx.fillText(tela.nome, 	this.x+100,	this.y);
+			ctx.fillText(this.nome, 	this.x+100, this.y+50);
+			ctx.fillText(this.res, 		this.x, 	this.y+50);
+			ctx.fillText(this.placar, 	this.x, 	this.y+100);
 			return ctx;
 		 }
 		 colisao(colide){
-			colide.topo = colide.y - colide.raio;  
-			colide.dir = colide.x + colide.raio;  
-			colide.fundo = colide.y + colide.raio;  
-			colide.esq = colide.x - colide.raio;  
-	
-			this.topo = this.y;  
-			this.dir = this.x + this.larg;  
-			this.fundo = this.y + this.alt;  
-			this.esq = this.x; 
-		
-			  	if(	this.topo < colide.fundo &&
-					this.dir > colide.esq && 
-					this.fundo > colide.topo &&
-					this.esq < colide.dir){
-						colisao = true;
-					}   			
-					return colisao
+			colide.topo = colide.y - colide.raio;
+			colide.dir = colide.x + colide.raio;
+			colide.fundo = colide.y + colide.raio;
+			colide.esq = colide.x - colide.raio;
+
+			this.topo = this.y;
+			this.dir = this.x + this.larg;
+			this.fundo = this.y + this.alt;
+			this.esq = this.x;
+
+			if(	this.topo < colide.fundo &&
+				this.dir > colide.esq &&
+				this.fundo > colide.topo &&
+				this.esq < colide.dir){
+					colisao = true;
+				}
+				return colisao
 		 }
 	}
-	//		    		|cor			|x           	|y			|larg		|alt	 	|raio   |vel   |nome 	|res   	|placar
-	let tela = new obj("DeepSkyBlue",	0, 			 	0,			canvas.width, canvas.height,null,null, 	null, 	null,  	null);
-	let p1 	   = new obj("purple", 	    tela.larg/2, 	tela.alt-20,null,		null, 		 30, 	 20,    null, 			null,  				0);
-	let p1Placar = new obj("blue", 	    tela.larg/8, 	tela.alt/8, null,		null,  		null,	 null,  tabela[0].nome,	tabela[0].placar,  	p1.placar);
+	//		    		|cor			|x           	|y			|larg		|alt	 	|raio   |vel   	|nome 				|res   			|placar		|vida
+	let tela = new obj("#FFFFCC",		0, 			 	0,		canvas.width, canvas.height, null,   null, 	null, 				null,  			null,		null);
+	let porta = new obj("black",   		tela.larg+5000,	0,			100,		100, 		 null, 	 velo,   null, 				null,			null,		null);
 
 	let torre = [];
 	for(i=0;i<=torres;i++){
-		torre[i]  = new obj("green", 	tela.larg,  	 0,			200,		100, 		 null, 	 -10,   null, 	null,  null);
+		torre[i]  = new obj("#FFCC00", 	tela.larg,  	 0,			200,		50, 		 null, 	 velo,   null, 				null,			null,		null);
 	}
 	let argola = [];
 	for(i=0;i<=argolas;i++){
-		argola[i]  = new obj("yellow", 	tela.larg,		 0,			null,		null, 		 30, 	 -10,   null, 	null,  null);
+		argola[i]  = new obj("yellow", 	tela.larg,		 0,			null,		null, 		 30, 	 velo,   null, 				null, 			null);
 	}
 	let cp = [];
 	for(i=0;i<=cps;i++){
-		cp[i]  = new obj("red", 		tela.larg,  	 0,			100,		100, 		 null, 	 -10,   null, 	null,  null);
+		cp[i]  = new obj("#FFCC00", 	tela.larg,  	 0,			100,		100, 		 null, 	 velo,   null, 				null, 		null,			null);
 	}
+
+	let p1 	   = new obj("purple", 	    tela.larg/2, 	tela.alt-20,null,		null, 		 30, 	 velo,    null, 			null,  			0,			3);
+	let tiro   = new obj("pink", 	    tela.larg/2, 	tela.alt-20,null,		null, 		 10, 	 velo,    null, 			null,  			0,			null);
+
+	let p1Placar = new obj("#6B4226", 	tela.larg/8, 	tela.alt/8, null,		null,  		null,	 null,  tabela[0].nome,	 tabela[0].placar, 	p1.placar,	null);
+
+	function comprar(compra, troca){
+		compra = prompt("Cuidado com as armadilhas!!!"+"\n\n"+
+		'deseja comprar mais vidas?'+"\n"+
+		'digite' +"\n"+
+		'1 para SIM' +"\n"+
+		'2 para NAO');
+		if(compra == 1){
+			troca = prompt(	"Cada vida custa 100 pontos"+"\n"+
+							"Digite quantas vidas quer comprar"
+						  );
+			vidas = p1Placar.placar/(troca*100);
+			p1.vida += vidas;
+			p1Placar.placar -= (troca*100);
+		}
+	}
+	function clear(){
+		p1.x = tela.larg/2;
+		p1.y = tela.alt-20;
+		espaco = false;
+		setaFrente = false;
+		setaTras = false;
+		disparo = false;
+	}
+	function fase1(){		
 	// cenario
-	function cenario(){
-	torre[0].x=tela.larg;		torre[0].y=	400;	
-	torre[1].x=tela.larg+ 300;	torre[1].y=	200;	
-	torre[2].x=tela.larg+ 600; 	torre[2].y=	200;	
+		tela.nome="DESERTO";
+		tela.cor="#FFFFCC"; 
+		p1Placar.cor="#6B4226";
+		porta.x=tela.larg+ 	5000;	
+		
+		torre[0].x=tela.larg;		torre[0].y=	400; torre[0].alt=200; torre[0].larg=400; torre[0].cor="#FFCC00";
+		torre[1].x=tela.larg+ 200;	torre[1].y=	200; torre[1].alt=200; torre[1].larg=400; torre[1].cor="#FFCC00";	
+		torre[2].x=tela.larg+ 750; 	torre[2].y=	100; torre[2].alt=400; torre[2].larg=200; torre[2].cor="#FFCC00";
 
-	argola[0].x=tela.larg+ 100;	argola[0].y=350;
-	argola[1].x=tela.larg+ 400;	argola[1].y=150;
-	argola[2].x=tela.larg+ 500;	argola[2].y=350;
+		cp[0].x=tela.larg+ 100; 	cp[0].y=500;	cp[0].y=200; cp[0].larg=100;	cp[0].cor="#FFCC00";
+		cp[1].x=tela.larg+ 500;		cp[1].y=400;	cp[1].y=200; cp[1].larg=100;	cp[1].cor="#FFCC00";
+		cp[2].x=tela.larg+ 800; 	cp[2].y=400;	cp[2].y=200; cp[2].larg=100;	cp[2].cor="#FFCC00";
+		
+		argola[0].x=tela.larg+ 100;	argola[0].y=300;
+		argola[1].x=tela.larg+ 500;	argola[1].y=450;
+		argola[2].x=tela.larg+ 900;	argola[2].y=50;
 
-	cp[0].x=tela.larg+ 200; 	cp[0].y=200;	
-	cp[1].x=tela.larg+ 500;		cp[1].y=400;	
-	cp[2].x=tela.larg+ 800; 	cp[2].y=400;	
+		//clear();
 	}
-	cenario();
+	function fase2(callback){
+		tela.nome="FLORESTA";
+		tela.cor = "#009900";
+		p1Placar.cor = "white";
+		porta.x=tela.larg+ 	5000;	
+
+		torre[0].x=tela.larg;			torre[0].y=	100; torre[0].alt=400; torre[0].larg=100; torre[0].cor="#003300";
+		torre[1].x=tela.larg+ 	200;	torre[1].y=	400; torre[1].alt=100; torre[1].larg=600; torre[1].cor="#003300";
+		torre[2].x=tela.larg+ 	600; 	torre[2].y=	200; torre[2].alt=400; torre[2].larg=100; torre[2].cor="#003300";
+
+		cp[0].x=tela.larg;				cp[0].y=200; cp[0].cor="#003300";
+		cp[1].x=tela.larg+ 		600;	cp[1].y=400; cp[1].cor="#003300";
+		cp[2].x=tela.larg+ 		1000; 	cp[2].y=400; cp[2].cor="#003300";
+
+		argola[0].x=tela.larg+ 	50;		argola[0].y=50;
+		argola[1].x=tela.larg+ 	400;	argola[1].y=350;
+		argola[2].x=tela.larg+ 	650;	argola[2].y=150;
+				
+		comprar(callback)
+		clear();
+	}	
+	function fase3(){
+		tela.nome="LABIRINTO";
+		tela.cor = "#999900";
+		p1Placar.cor = "white";
+		porta.x=tela.larg+ 	5200;	
+
+		torre[0].x=tela.larg;		torre[0].y=	100; torre[0].alt=400; torre[0].larg=200;
+		torre[1].x=tela.larg+ 400;	torre[1].y=	200; torre[1].alt=400; torre[1].larg=200;
+		torre[2].x=tela.larg+ 800; 	torre[2].y=	0;	 torre[2].alt=500; torre[2].larg=200;
+
+		cp[0].x=tela.larg; 		 	cp[0].y=200; cp[0].larg=200;
+		cp[1].x=tela.larg+ 400;		cp[1].y=200; cp[1].larg=200;
+		cp[2].x=tela.larg+ 800; 	cp[2].y=200; cp[2].larg=200;
+
+		argola[0].x=tela.larg+ 100;	argola[0].y=50;
+		argola[1].x=tela.larg+ 300;	argola[1].y=450;
+		argola[2].x=tela.larg+ 500;	argola[2].y=150;
+
+		clear();
+	}
+	function faseF(){
+		tela.nome="CASTELO";
+		tela.cor = "#8FBC8F";
+		p1Placar.cor = "white";
+		porta.x=tela.larg+ 	5200;	
+
+		torre[0].x=tela.larg;		torre[0].y=	0;	 torre[0].alt=500; torre[0].cor="#426F42";
+		torre[1].x=tela.larg+ 300;	torre[1].y=	100; torre[1].alt=400; torre[1].cor="#426F42"; torre[1].larg=300;
+		torre[2].x=tela.larg+ 800; 	torre[2].y=	0;	 torre[2].alt=500; torre[2].cor="#426F42";
+
+		cp[0].x=tela.larg; 		 	cp[0].y=200; cp[0].larg=200;		cp[0].cor="#426F42";
+		cp[1].x=tela.larg+ 300;		cp[1].y=400; cp[1].larg=200;		cp[1].cor="#426F42"; 	cp[1].larg=300;
+		cp[2].x=tela.larg+ 800; 	cp[2].y=400; cp[2].larg=200;		cp[2].cor="#426F42";
+
+		argola[0].x=tela.larg+ 300;	argola[0].y=50;
+		argola[1].x=tela.larg+ 600;	argola[1].y=50;
+		argola[2].x=tela.larg+ 900;	argola[2].y=550;
+
+		//clear();
+		alert("fase final!!!");
+	}
+	fase1();
+	function trocaFase(){
+		if(tela.cor == "#FFFFCC"){
+			fase2();
+		} else if(tela.cor == "#009900"){
+			fase3();
+		} else if(tela.cor == "#999900"){
+			faseF();			
+		} else if(tela.cor == "#8FBC8F"){
+			alert('Parabens'+"\n"+'Voce venceu o desafio final!!!');
+			ranking();
+			reset();
+		}
+	}
 
 //_3.2_///////////////////////////// movimento p1
 	let setaFrente = false;
-	let setaTras = false;	
+	let setaTras = false;
 	let espaco = false;
+	let disparo = false;
 
 	window.addEventListener('keydown', teclaOn);
-	function teclaOn(e) {    
-		if (e.keyCode == 32){
-			espaco = true;   
-		} else if (e.keyCode == 37){
-			setaFrente = true; 
-		} else if (e.keyCode == 39){
-			setaTras = true;      
+	function teclaOn(e) {
+		if (e.keyCode == 32){ //backspace
+			espaco = true;
+		} else if (e.keyCode == 37){ //setaTras
+			setaTras = true;
+		} else if (e.keyCode == 39){ //setaFrente
+			setaFrente = true;
+		} else if (e.keyCode == 67){ // letra c do teclado
+			disparo = true;
 		}
-	} 
+	}
 	window.addEventListener('keyup', teclaOff);
-	function teclaOff(e) {    
-		if (e.keyCode == 32){
-			espaco = false;    
-		} else if (e.keyCode == 37){
-			setaFrente = false; 
-		} else if (e.keyCode == 39){
-			setaTras = false;      
+	function teclaOff(e) {
+		if (e.keyCode == 32){ //backspace
+			espaco = false;
+		} else if (e.keyCode == 37){ //setaTras
+			setaTras = false;
+		} else if (e.keyCode == 39){ //setaFrente
+			setaFrente = false;
+		} else if (e.keyCode == 67){ // letra c do teclado
+			disparo = false;
 		}
 	}
 //_3.1_////////////////////////////// recomeco
-	function reset() {  
+	function reset() {
 		//window.location.reload()
-		p1.x = tela.larg/2; 	 
-		p1.y = tela.alt-20;
-		p1Placar.placar = 0;
-		cenario();
-		espaco = false;
-		setaFrente = false; 
-		setaTras = false;  
+		clear();		
+		fase1();		
 	 }
+
 //_3_///////////////////////////// ranking
 	 function lista(){
-		alert(	tabela[0].placar+' | '+tabela[0].nome+"\n"+ 
-				tabela[1].placar+' | '+tabela[1].nome+"\n"+ 
-				tabela[2].placar+' | '+tabela[2].nome+"\n"+ 
-				tabela[3].placar+' | '+tabela[3].nome+"\n"+ 
-				tabela[4].placar+' | '+tabela[4].nome+"\n"+ 
-				tabela[5].placar+' | '+tabela[5].nome		
+		alert(	tabela[0].placar+' | '+tabela[0].nome+"\n"+
+				tabela[1].placar+' | '+tabela[1].nome+"\n"+
+				tabela[2].placar+' | '+tabela[2].nome+"\n"+
+				tabela[3].placar+' | '+tabela[3].nome+"\n"+
+				tabela[4].placar+' | '+tabela[4].nome+"\n"+
+				tabela[5].placar+' | '+tabela[5].nome
 		);
 	 }
 	 function ranking(){
@@ -177,87 +300,129 @@ alert("USE A TECLA ESPACO PARA FLUTUAR")
 				return 0;
 			});
 			window.localStorage.setItem('bdAdv', JSON.stringify(tabela));
-			
+
 			lista();
 
 			p1Placar.nome=tabela[0].nome;
 			p1Placar.res=tabela[0].placar;
 	 }
-//_2.2_///////////////////////////// atualiza	
-	function atualiza() {       
+//_2.2_///////////////////////////// atualiza
+	function atualiza() {
  // movimento atualiza
-  // movimento p1 
+  // movimento p1
 		if (p1.y > tela.alt-(2*p1.raio)){
 			p1.y = tela.alt-(2*p1.raio);
-		} 
+			tiro.y = tela.alt-(2*p1.raio);
+		}
 		if (p1.y < tela.y+(2*p1.raio)){
 			p1.y = tela.y+(2*p1.raio);
-		} 
-		if (espaco) { // move p1
-			p1.y -= p1.raio;  
-		} else { 
-			p1.y += p1.raio;
-		}		
-	// movimento torre
-	if (setaFrente) {  // movimento cenario
-		for(i in torre){ 
-			torre[i].x -= torre[i].vel; 
-			argola[i].x -= argola[i].vel;
-			cp[i].x -= cp[i].vel;
-		}	
-	} else if (setaTras) {
-		for(i in torre){
-			torre[i].x += torre[i].vel; 
-			argola[i].x += argola[i].vel;
-			cp[i].x += cp[i].vel;
+			tiro.y = tela.y+(2*p1.raio);
 		}
-	} 
+		if (espaco) { // move p1
+			p1.y -= p1.raio;
+			tiro.y -= p1.raio;
+		} else {
+			p1.y += p1.raio;
+			tiro.y += p1.raio;
+		}
+	// movimento torre
+	if (setaFrente) {  // movimento cenario	
+		for(i in torre){
+			if (p1.x == tela.larg/2){
+				torre[i].x += torre[i].vel;
+				argola[i].x += argola[i].vel;
+				cp[i].x += cp[i].vel;
+				porta.x += porta.vel/3;	
+			} else {
+				p1.x -= p1.vel;
+				tiro.x -= p1.vel;
+			}
+		}
+	} else if (setaTras) {
+		if(p1.x <= tela.x + (4*p1.raio)){
+			p1.x = p1.x;
+			tiro.x = tiro.x;
+		} else {
+			p1.x += p1.vel;
+			tiro.x += tiro.vel;
+		}					
+	}
+	if(disparo){
+		tiro.x -= tiro.vel;
+	} else if (!disparo){
+		tiro.x = p1.x;
+	}
+	if (tiro.x >= p1.x + p1.raio*3){
+		tiro.x = p1.x;
+	}
 	//progressao do cenario
+	if (porta.x + porta.larg == 0){
+		porta.x = tela.larg + 5000;
+	};
 	for(i in torre){
 		if (torre[i].x + torre[i].larg == 0){
 			torre[i].x = tela.larg;
 		};
-	}	
+	}
 	for(i in argola){
 		if (argola[i].x + argola[i].larg == 0){
 			argola[i].x = tela.larg;
 		};
-	}	
+	}
 	for(i in cp){
-		cp[i].y += ((p1.y - (cp[i].y + cp[i].alt / 2)) * 0.01);   
+		cp[i].y += ((p1.y - (cp[i].y + cp[i].alt / 2)) * 0.01);
 		if (cp[i].x + cp[i].larg == 0){
 			cp[i].x = tela.larg;
 		};
-	}	
+	}
   	// Colisao atualiza
 		for(i in torre){
 			if (torre[i].colisao(p1)) {
 				colisao = false;
-				p1.y = torre[i].y - p1.raio;								
+				p1.y = torre[i].y - p1.raio;
+				tiro.y = torre[i].y - p1.raio;
 			}
-		}					
+		}
 		for(i in cp){
 			if (cp[i].colisao(p1)) {
 				colisao = false;
-				alert("voce perdeu!!!");
-				ranking();
-				reset(); 								
+				p1.vida -= 1;
+				alert("kabum!!!");	
+				reset();			
 			}
-		}								
+			if (p1.vida == 0){
+				alert("Voce perdeu!!!");
+				ranking();
+				reset();
+				p1.vida = 3;
+			}
+			if (cp[i].colisao(tiro)) {
+				colisao = false;
+				cp[i].x = 3*tela.larg/2;
+				p1Placar.placar += 10;
+			}
+		
+		}
  	 // pontuacao atualiza
 		for(i in argola){
 			if (argola[i].colisao(p1)) {
 				colisao = false;
 				p1Placar.placar += 10;
 				argola[i].x = tela.larg;
-			}				
-		} 	
-	}
+			}
+		}
+	//troca de fases	
+		if(porta.colisao(p1)){
+			alert('Parabens'+"\n"+'Voce passou esta fase!!!');
+			trocaFase();
+			}
+		}
+	
+	
 //_2.1_///////////////////////////// desenha
-	function desenha() {  
+	function desenha() {
 		tela.retangulo();
-		p1.circulo();
-		p1Placar.text();
+
 
 		for(i in torre){
 			torre[i].retangulo();
@@ -268,9 +433,14 @@ alert("USE A TECLA ESPACO PARA FLUTUAR")
 		for(i in cp){
 			cp[i].retangulo();
 		}
+
+		p1.circulo();
+		tiro.circulo();
+		p1Placar.text();
+		porta.retangulo();
 	}
 //_1_/////////////////////////////// loop
-	function Loop() { 
+	function Loop() {
 		desenha()
 		atualiza()
 	}
--- adventure/script.js
+++ adventure/script.js
@@ -1,11 +1,12 @@
 const canvas = document.getElementById('canvas');
-canvas.width = 1100; //window.innerWidth;
+canvas.width = 1200; //window.innerWidth;
 canvas.height = 600 //window.innerHeight;
 const ctx = canvas.getContext('2d');
 let colisao = false;
 let torres = 2;
 let argolas = 2;
 let cps = 2;
+let velo = -5;
 
 // conexao localstorage
 if(localStorage.hasOwnProperty('bdAdv')){
@@ -35,126 +36,139 @@ alert("USE A TECLA ESPACO PARA FLUTUAR")
 			this.raio = raio;
 			this.vel = vel;
 			this.nome = nome;
-			this.res = res;	
+			this.res = res;
 			this.placar = placar;
 		}
 		retangulo(){
-			ctx.fillStyle = this.cor;                                     
-			ctx.fillRect(this.x, this.y, this.larg, this.alt); 
+			ctx.fillStyle = this.cor;
+			ctx.fillRect(this.x, this.y, this.larg, this.alt);
 			return ctx
 		}
 		circulo(){
-			ctx.fillStyle = this.cor;  
-			ctx.beginPath();  
-			ctx.arc(this.x, this.y, this.raio, 0, Math.PI * 2, true);    // x, y, raio, angulo, fimAngulo, rotacao)  
+			ctx.fillStyle = this.cor;
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.raio, 0, Math.PI * 2, true);    // x, y, raio, angulo, fimAngulo, rotacao)
 			ctx.fill();
 			return ctx;
 		}
 		text(){                         // função para desenhar placar
-			ctx.fillStyle = this.cor;  
-			ctx.font = '35px sans-serif';  
+			ctx.fillStyle = this.cor;
+			ctx.font = '35px sans-serif';
 			ctx.fillText(this.nome, this.x+100, this.y);
 			ctx.fillText(this.res, this.x, this.y);
 			ctx.fillText(this.placar, this.x, this.y+50);
 			return ctx;
 		 }
 		 colisao(colide){
-			colide.topo = colide.y - colide.raio;  
-			colide.dir = colide.x + colide.raio;  
-			colide.fundo = colide.y + colide.raio;  
-			colide.esq = colide.x - colide.raio;  
-	
-			this.topo = this.y;  
-			this.dir = this.x + this.larg;  
-			this.fundo = this.y + this.alt;  
-			this.esq = this.x; 
-		
+			colide.topo = colide.y - colide.raio;
+			colide.dir = colide.x + colide.raio;
+			colide.fundo = colide.y + colide.raio;
+			colide.esq = colide.x - colide.raio;
+
+			this.topo = this.y;
+			this.dir = this.x + this.larg;
+			this.fundo = this.y + this.alt;
+			this.esq = this.x;
+
 			  	if(	this.topo < colide.fundo &&
-					this.dir > colide.esq && 
+					this.dir > colide.esq &&
 					this.fundo > colide.topo &&
 					this.esq < colide.dir){
 						colisao = true;
-					}   			
+					}
 					return colisao
 		 }
 	}
-	//		    		|cor			|x           	|y			|larg		|alt	 	|raio   |vel   |nome 	|res   	|placar
-	let tela = new obj("DeepSkyBlue",	0, 			 	0,			canvas.width, canvas.height,null,null, 	null, 	null,  	null);
-	let p1 	   = new obj("purple", 	    tela.larg/2, 	tela.alt-20,null,		null, 		 30, 	 20,    null, 			null,  				0);
-	let p1Placar = new obj("blue", 	    tela.larg/8, 	tela.alt/8, null,		null,  		null,	 null,  tabela[0].nome,	tabela[0].placar,  	p1.placar);
+	//		    		|cor			|x           	|y			|larg		|alt	 	|raio   |vel   	|nome 				|res   			|placar
+	let tela = new obj("DeepSkyBlue",	0, 			 	0,		canvas.width, canvas.height, null,   null, 	null, 				null,  			null);
+	let porta = new obj("brown",   		tela.larg,		0,			200,		200, 		 null, 	 velo,   null, 				null,			null);
 
 	let torre = [];
 	for(i=0;i<=torres;i++){
-		torre[i]  = new obj("green", 	tela.larg,  	 0,			200,		100, 		 null, 	 -10,   null, 	null,  null);
+		torre[i]  = new obj("green", 	tela.larg,  	 0,			200,		50, 		 null, 	 velo,   null, 				null,			null);
 	}
 	let argola = [];
 	for(i=0;i<=argolas;i++){
-		argola[i]  = new obj("yellow", 	tela.larg,		 0,			null,		null, 		 30, 	 -10,   null, 	null,  null);
+		argola[i]  = new obj("yellow", 	tela.larg,		 0,			null,		null, 		 30, 	 velo,   null, 				null, 			null);
 	}
 	let cp = [];
 	for(i=0;i<=cps;i++){
-		cp[i]  = new obj("red", 		tela.larg,  	 0,			100,		100, 		 null, 	 -10,   null, 	null,  null);
+		cp[i]  = new obj("red", 		tela.larg,  	 0,			100,		100, 		 null, 	 velo,   null, 				null, 			null);
 	}
+
+	let p1 	   = new obj("purple", 	    tela.larg/2, 	tela.alt-20,null,		null, 		 30, 	 velo,    null, 			null,  			0);
+	let tiro   = new obj("pink", 	    tela.larg/2, 	tela.alt-20,null,		null, 		 10, 	 velo,    null, 			null,  			0);
+
+	let p1Placar = new obj("blue", 	    tela.larg/8, 	tela.alt/8, null,		null,  		null,	 null,  tabela[0].nome,	 tabela[0].placar, 	p1.placar);
+
 	// cenario
 	function cenario(){
-	torre[0].x=tela.larg;		torre[0].y=	400;	
-	torre[1].x=tela.larg+ 300;	torre[1].y=	200;	
-	torre[2].x=tela.larg+ 600; 	torre[2].y=	200;	
+		porta.x=tela.larg+ 	5000;	porta.y=	400;
 
-	argola[0].x=tela.larg+ 100;	argola[0].y=350;
-	argola[1].x=tela.larg+ 400;	argola[1].y=150;
-	argola[2].x=tela.larg+ 500;	argola[2].y=350;
+		torre[0].x=tela.larg;		torre[0].y=	150;
+		torre[1].x=tela.larg+ 400;	torre[1].y=	400;
+		torre[2].x=tela.larg+ 800; 	torre[2].y=	300;
 
-	cp[0].x=tela.larg+ 200; 	cp[0].y=200;	
-	cp[1].x=tela.larg+ 500;		cp[1].y=400;	
-	cp[2].x=tela.larg+ 800; 	cp[2].y=400;	
+		argola[0].x=tela.larg+ 100;	argola[0].y=100;
+		argola[1].x=tela.larg+ 500;	argola[1].y=350;
+		argola[2].x=tela.larg+ 900;	argola[2].y=250;
+
+		cp[0].x=tela.larg+ 200; 	cp[0].y=200;
+		cp[1].x=tela.larg+ 600;		cp[1].y=400;
+		cp[2].x=tela.larg+ 1000; 	cp[2].y=400;
 	}
 	cenario();
 
 //_3.2_///////////////////////////// movimento p1
 	let setaFrente = false;
-	let setaTras = false;	
+	let setaTras = false;
 	let espaco = false;
+	let disparo = false;
 
 	window.addEventListener('keydown', teclaOn);
-	function teclaOn(e) {    
-		if (e.keyCode == 32){
-			espaco = true;   
-		} else if (e.keyCode == 37){
-			setaFrente = true; 
-		} else if (e.keyCode == 39){
-			setaTras = true;      
+	function teclaOn(e) {
+		if (e.keyCode == 32){ //backspace
+			espaco = true;
+		} else if (e.keyCode == 37){ //setaTras
+			setaTras = true;
+		} else if (e.keyCode == 39){ //setaFrente
+			setaFrente = true;
+		} else if (e.keyCode == 67){ // letra c do teclado
+			disparo = true;
 		}
-	} 
+	}
 	window.addEventListener('keyup', teclaOff);
-	function teclaOff(e) {    
-		if (e.keyCode == 32){
-			espaco = false;    
-		} else if (e.keyCode == 37){
-			setaFrente = false; 
-		} else if (e.keyCode == 39){
-			setaTras = false;      
+	function teclaOff(e) {
+		if (e.keyCode == 32){ //backspace
+			espaco = false;
+		} else if (e.keyCode == 37){ //setaTras
+			setaTras = false;
+		} else if (e.keyCode == 39){ //setaFrente
+			setaFrente = false;
+		} else if (e.keyCode == 67){ // letra c do teclado
+			disparo = false;
 		}
 	}
 //_3.1_////////////////////////////// recomeco
-	function reset() {  
+	function reset() {
 		//window.location.reload()
-		p1.x = tela.larg/2; 	 
+		p1.x = tela.larg/2;
 		p1.y = tela.alt-20;
 		p1Placar.placar = 0;
 		cenario();
 		espaco = false;
-		setaFrente = false; 
-		setaTras = false;  
+		setaFrente = false;
+		setaTras = false;
+		disparo = false;
 	 }
 //_3_///////////////////////////// ranking
 	 function lista(){
-		alert(	tabela[0].placar+' | '+tabela[0].nome+"\n"+ 
-				tabela[1].placar+' | '+tabela[1].nome+"\n"+ 
-				tabela[2].placar+' | '+tabela[2].nome+"\n"+ 
-				tabela[3].placar+' | '+tabela[3].nome+"\n"+ 
-				tabela[4].placar+' | '+tabela[4].nome+"\n"+ 
-				tabela[5].placar+' | '+tabela[5].nome		
+		alert(	tabela[0].placar+' | '+tabela[0].nome+"\n"+
+				tabela[1].placar+' | '+tabela[1].nome+"\n"+
+				tabela[2].placar+' | '+tabela[2].nome+"\n"+
+				tabela[3].placar+' | '+tabela[3].nome+"\n"+
+				tabela[4].placar+' | '+tabela[4].nome+"\n"+
+				tabela[5].placar+' | '+tabela[5].nome
 		);
 	 }
 	 function ranking(){
@@ -177,87 +191,118 @@ alert("USE A TECLA ESPACO PARA FLUTUAR")
 				return 0;
 			});
 			window.localStorage.setItem('bdAdv', JSON.stringify(tabela));
-			
+
 			lista();
 
 			p1Placar.nome=tabela[0].nome;
 			p1Placar.res=tabela[0].placar;
 	 }
-//_2.2_///////////////////////////// atualiza	
-	function atualiza() {       
+//_2.2_///////////////////////////// atualiza
+	function atualiza() {
  // movimento atualiza
-  // movimento p1 
+  // movimento p1
 		if (p1.y > tela.alt-(2*p1.raio)){
 			p1.y = tela.alt-(2*p1.raio);
-		} 
+			tiro.y = tela.alt-(2*p1.raio);
+		}
 		if (p1.y < tela.y+(2*p1.raio)){
 			p1.y = tela.y+(2*p1.raio);
-		} 
-		if (espaco) { // move p1
-			p1.y -= p1.raio;  
-		} else { 
-			p1.y += p1.raio;
-		}		
-	// movimento torre
-	if (setaFrente) {  // movimento cenario
-		for(i in torre){ 
-			torre[i].x -= torre[i].vel; 
-			argola[i].x -= argola[i].vel;
-			cp[i].x -= cp[i].vel;
-		}	
-	} else if (setaTras) {
-		for(i in torre){
-			torre[i].x += torre[i].vel; 
-			argola[i].x += argola[i].vel;
-			cp[i].x += cp[i].vel;
+			tiro.y = tela.y+(2*p1.raio);
 		}
-	} 
+		if (espaco) { // move p1
+			p1.y -= p1.raio;
+			tiro.y -= p1.raio;
+		} else {
+			p1.y += p1.raio;
+			tiro.y += p1.raio;
+		}
+	// movimento torre
+	if (setaFrente) {  // movimento cenario	
+		for(i in torre){
+			if (p1.x == tela.larg/2){
+				torre[i].x += torre[i].vel;
+				argola[i].x += argola[i].vel;
+				cp[i].x += cp[i].vel;
+				porta.x += porta.vel/3;	
+			} else {
+				p1.x -= p1.vel;
+				tiro.x -= p1.vel;
+			}
+		}
+	} else if (setaTras) {
+		if(p1.x <= tela.x + (4*p1.raio)){
+			p1.x = p1.x;
+			tiro.x = tiro.x;
+		} else {
+			p1.x += p1.vel;
+			tiro.x += tiro.vel;
+		}					
+	}
+	if(disparo){
+		tiro.x -= tiro.vel;
+	} else if (!disparo){
+		tiro.x = p1.x;
+	}
+	if (tiro.x >= p1.x + p1.raio*3){
+		tiro.x = p1.x;
+	}
 	//progressao do cenario
 	for(i in torre){
 		if (torre[i].x + torre[i].larg == 0){
 			torre[i].x = tela.larg;
 		};
-	}	
+	}
 	for(i in argola){
 		if (argola[i].x + argola[i].larg == 0){
 			argola[i].x = tela.larg;
 		};
-	}	
+	}
 	for(i in cp){
-		cp[i].y += ((p1.y - (cp[i].y + cp[i].alt / 2)) * 0.01);   
+		cp[i].y += ((p1.y - (cp[i].y + cp[i].alt / 2)) * 0.01);
 		if (cp[i].x + cp[i].larg == 0){
 			cp[i].x = tela.larg;
 		};
-	}	
+	}
   	// Colisao atualiza
 		for(i in torre){
 			if (torre[i].colisao(p1)) {
 				colisao = false;
-				p1.y = torre[i].y - p1.raio;								
+				p1.y = torre[i].y - p1.raio;
+				tiro.y = torre[i].y - p1.raio;
 			}
-		}					
+		}
 		for(i in cp){
 			if (cp[i].colisao(p1)) {
 				colisao = false;
 				alert("voce perdeu!!!");
 				ranking();
-				reset(); 								
+				reset();
 			}
-		}								
+			if (cp[i].colisao(tiro)) {
+				colisao = false;
+				cp[i].x = 3*tela.larg/2;
+				p1Placar.placar += 10;
+			}
+		}
  	 // pontuacao atualiza
 		for(i in argola){
 			if (argola[i].colisao(p1)) {
 				colisao = false;
 				p1Placar.placar += 10;
 				argola[i].x = tela.larg;
-			}				
-		} 	
+			}
+		}
+	//fim de fase	
+		if(porta.colisao(p1)){
+			alert('Parabens'+"\n"+'Voce venceu esta fase!!!');
+			reset();
+		}
 	}
+	
 //_2.1_///////////////////////////// desenha
-	function desenha() {  
+	function desenha() {
 		tela.retangulo();
-		p1.circulo();
-		p1Placar.text();
+
 
 		for(i in torre){
 			torre[i].retangulo();
@@ -268,9 +313,14 @@ alert("USE A TECLA ESPACO PARA FLUTUAR")
 		for(i in cp){
 			cp[i].retangulo();
 		}
+
+		p1.circulo();
+		tiro.circulo();
+		p1Placar.text();
+		porta.retangulo();
 	}
 //_1_/////////////////////////////// loop
-	function Loop() { 
+	function Loop() {
 		desenha()
 		atualiza()
 	}
